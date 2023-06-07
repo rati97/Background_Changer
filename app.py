f4085f5db592ed4be2700771493f8a7222f2fd89
Rati Chkhetia <rchkhetia25@gmail.com>
2023-06-08 01:21:43 +0400

    Add background changer into website

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+
+from PIL import Image
+import io
+import base64
+
+from functions import change_background as cb
+
 
 app = Flask(__name__)
 
@@ -11,3 +18,45 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/change_background", methods=["POST"])
+def change_background():
+    data = request.get_json()
+
+    # Parse the image data from the request
+    selfie_data = base64.b64decode(data["selfie"].split(",")[1])
+
+    if data["background"] is not None:
+        background_data = base64.b64decode(data["background"].split(",")[1])
+        background = Image.open(io.BytesIO(background_data))
+    else:
+        # convert hex to rgb
+        bg_color = data["bg_color"]
+        bg_color = bg_color.lstrip("#")
+        bg_color = tuple(int(bg_color[i : i + 2], 16) for i in (0, 2, 4))
+
+        # Create image filled with color
+        background = Image.new("RGB", (100, 100))
+        background.paste(bg_color, (0, 0, background.size[0], background.size[1]))
+
+    # Load the images into PIL
+    selfie = Image.open(io.BytesIO(selfie_data))
+
+    # Handle the other form data
+    resize_option = data["resize_option"]
+
+    # Change background
+    result = cb(selfie, background, resize_option)
+
+    # Convert the result to base64 to return in the response
+    output_bytes = io.BytesIO()
+    result.save(output_bytes, format="JPEG")
+    output_bytes.seek(0)
+    output_base64 = base64.b64encode(output_bytes.read())
+
+    # Prepend the data type prefix
+    output_base64_string = "data:image/jpeg;base64," + output_base64.decode("utf-8")
+
+    # Return the result
+    return jsonify({"result": output_base64_string})
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -6,3 +6,86 @@ function loadImage(event, previewId) {
     };
     reader.readAsDataURL(event.target.files[0]);
 }
+
+
+document.getElementById('change-background-form').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent the form from submitting normally
+
+    const selfieInput = document.getElementById('selfie');
+    if (selfieInput.files.length === 0) {
+        alert("Please upload a selfie before submitting");
+        return;
+    }
+    const backgroundInput = document.getElementById('background');
+    const bgColorInput = document.getElementById('bg-color');
+    const resizeOptionInput = document.getElementById('resize-option');
+
+    
+
+    const selfieFile = selfieInput.files[0];
+    const backgroundFile = backgroundInput.files[0];
+
+    // Create a promise to handle reading the file
+    function readFile(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onloadend = function() {
+                resolve(reader.result);
+            };
+
+            reader.onerror = reject;
+            if (file instanceof Blob) {
+                reader.readAsDataURL(file);
+            }else{
+                resolve(null)
+            }
+        });
+    }
+
+    // When both files are fully read
+    Promise.all([readFile(selfieFile), readFile(backgroundFile)]).then(function(results) {
+        // Extract the file data from the data URLs
+        const selfieData = results[0];
+        const backgroundData = results[1];
+        
+
+        // Prepare the data to send to the server
+        const data = {
+            'selfie': selfieData,
+            'background': backgroundData,
+            'bg_color': bgColorInput.value,
+            'resize_option': resizeOptionInput.value,
+        };
+
+        // Send the data to the server
+        let url = document.getElementsByClassName("submit-url");
+        url = url[0].id
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(function(response) {
+            // Handle the server's response
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('An error occurred: ' + response.statusText);
+            }
+        }).then(function(data) {
+            if (data && data.result) {
+                console.log('Background changed successfully');
+                
+                const result_image = document.getElementById('result-image');
+                result_image.style.display = "block"
+                result_image.src = data.result;
+            } else {
+                throw new Error('An error occurred: The server\'s response did not include a result');
+            }
+        }).catch(function(error) {
+            console.error(error);
+        });
+    });
+});
